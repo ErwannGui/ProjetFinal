b7229052172c72091965e4ee37d510a002badda7
PC-MSI-GEOFFREY\Geoffrey <geoffrey.moreau@ynov.com>
2017-05-17 16:36:12 +0200

    About GM

---
+++ about.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>À propos de nous</title>
+        <meta charset="utf-8"/>
+        <link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
+	</head>
+	<body>
+		<?php include "headerFront.php"; ?>
+		<div id="container">
+			
+		</div>
+	</body>
+</html>
