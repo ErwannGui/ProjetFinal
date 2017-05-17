82fe0363ea1a657cec3e0a7a216fb51b38b67866
ErwannGui <erwann.guillevic@ynov.com>
2017-05-17 16:12:59 +0200

    headback

--- headerBack.php
+++ headerBack.php
@@ -2,4 +2,13 @@
     <div id="headerContainerOne">
         <img src="images/logo.png" alt="logo" title="Logo Chez Fifi">
     </div>
+    <div id="headerContainerTwo">
+            <div><a href="#">Menus</a></div>
+            <div><a href="#">Articles</a></div>
+            <div><a href="#">Patenaires</a></div>
+            <div><a href="#">Evenements</a></div>
+            <div><a href="#">Contact</a></div>
+            <div><a href="#">Autres</a></div>
+            <div><a href="#">Réseaux</a></div>
+        </div>
 </header>
