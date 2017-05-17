eb46da31d15f978b570cee936dda9ce0d769437d
AntoninDouillard <antonin.douillard@ynov.com>
2017-05-17 14:17:10 +0200

    Commit AD 2

---
+++ blog.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <meta charset="utf-8" />
+        <title>Actualités</title>
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+
+    <body>
+        
+        <?php include "headerFront.php"; ?>
+        
+        
+        
+    </body>
+
+</html>
