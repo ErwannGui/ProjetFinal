bb6d8e82a2064cab5b1e19b2e969afbac9990a68
AntoninDouillard <antonin.douillard@ynov.com>
2017-05-18 11:21:29 +0200

    Page blog

--- blog.php
+++ blog.php
@@ -15,12 +15,47 @@
         <div id="container">
             
             <div id="blogBackground">
+                
                 <div id="blogBackgroundImage">
-                    <img src="images/camtar.png" alt="Food truck" title="Food truck">
+                    <img src="images/blog_home.png" alt="Food truck" title="Food truck">
                 </div>
                 <div id="blogBackgroundTitle">
-                    <h1>BIENVENUE CHEZ FIFI</h1>
+                    <h1>BLOG ET ACTUALITES</h1>
                 </div>
+                <div id="blogBackgroundArrow">
+                    <a href="#blogArticles"><svg enable-background="new 0 0 26 26" id="Layer_1" version="1.1" viewBox="0 0 26 26" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><polygon points="0.046,2.582 2.13,0.498 12.967,11.334 23.803,0.498 25.887,2.582 12.967,15.502  "/><polygon points="0.046,13.582 2.13,11.498 12.967,22.334 23.803,11.498 25.887,13.582 12.967,26.502"/></g></svg></a>
+                </div>
+                
+            </div>
+            
+            <div id="blogArticles">
+                
+                <div id="blogArticlesTitle">
+                    <h2>Actualités</h2>
+                </div>
+                <div id="blogArticlesArticles">
+                    <div>
+                        <a href="#"><img src="images/articles/articleOne.jpg" alt="Food truck" title="Food truck"></a>
+                        <a href="#"><p>Conseils pour bien gérer son food truck</p></a>
+                    </div>
+                    <div>
+                        <a href="#"><img src="images/articles/articleTwo.jpg" alt="Food truck" title="Food truck"></a>
+                        <a href="#"><p>Etre traiteur en food truck</p></a>
+                    </div>
+                    <div>
+                        <a href="#"><img src="images/articles/articleThree.jpg" alt="Food truck" title="Food truck"></a>
+                        <a href="#"><p>La cuisine créole : nouvelle à Nantes</p></a>
+                    </div>
+                    <div>
+                        <a href="#"><img src="images/articles/articleFour.jpg" alt="Food truck" title="Food truck"></a>
+                        <a href="#"><p>Points stratégiques de l’activité d’un food truck</p></a>
+                    </div>
+                    <div>
+                        <a href="#"><img src="images/articles/articleFive.jpg" alt="Food truck" title="Food truck"></a>
+                        <a href="#"><p>L’évolution du marché des food trucks</p></a>
+                    </div>
+                </div>
+                
             </div>
                  
         </div>
