0328c4df2471dd86ac144ef50d2411c9795ca868
Shadowkyu <quentin.dijoux@ynov.com>
2017-05-18 17:30:55 +0200

    update QD 01

--- index.php
+++ index.php
@@ -10,7 +10,108 @@
 		<?php include "headerFront.php"; ?>
 		
         <div id="container">
-			
+            <div id="blogBackground">
+
+                <div id="backgroundImage">
+                </div>
+                <div id="blogBackgroundTitle">
+                    <h1>Acceuil</h1>
+                </div>
+                <div id="blogBackgroundArrow">
+                    <a href="#blogArticles"><svg enable-background="new 0 0 26 26" id="Layer_1" version="1.1" viewBox="0 0 26 26" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><polygon points="0.046,2.582 2.13,0.498 12.967,11.334 23.803,0.498 25.887,2.582 12.967,15.502  "/><polygon points="0.046,13.582 2.13,11.498 12.967,22.334 23.803,11.498 25.887,13.582 12.967,26.502"/></g></svg></a>
+                </div>
+            </div>
+            <div class="index-container" >
+                <div class=text-container">
+                    <h2> Description </h2>
+                        <p> Lorem ipsum dolor sit amet, consectetuer adipiscing elit.Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis
+                            parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
+                            Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum
+                            felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula,
+                            porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus
+                            varius laoreet. </p>
+                </div>
+                <div class="image-container">
+                    <img src="images/blog_home.png">
+                </div>
+                <div class="formules">
+                    <h2>Nos formules</h2>
+                    <div>
+                       <img src="#">
+                       <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis
+                           parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec</p>
+                    </div>
+                    <div>
+                        <img src="#">
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis
+                            parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec</p>
+                    </div>
+                    <div>
+                        <img src="#">
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis
+                            parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec</p>
+                    </div>
+                    <a href="#"> Nos Formules</a>
+                </div>
+                <div class="events">
+                    <h2>Events</h2>
+                    <h3>Interventions</h3>
+                    <div class="mariage">
+                        <svg></svg>
+                        <p> porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus
+                            varius laoreet.</p>
+                    </div>
+                    <div class="anniversaire">
+                        <svg></svg>
+                        <p> porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus
+                            varius laoreet.</p>
+                    </div>
+                    <div class="baptême">
+                        <svg></svg>
+                        <p> porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus
+                            varius laoreet.</p>
+                    </div>
+                    <a href="#"> Evenements</a>
+                </div>
+                <div class="information">
+                    <div>
+                        <div>
+                            <p>Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
+                                Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo,
+                            </p>
+                        </div>
+                        <div>
+                            <p>Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
+                                Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo,
+                            </p>
+                        </div>
+                        <div>
+                            <p>Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
+                                Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo,
+                            </p>
+                        </div>
+                        <div>
+                            <p>Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
+                                Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo,
+                            </p>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <p>Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
+                                Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo,
+                            </p>
+                        </div>
+                        <div>
+                            <p>Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
+                                Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo,
+                            </p>
+                        </div>
+                    </div>
+                    <p> MAP </p>
+                </div>
+            </div>
+
 		</div>
         
 		<?php include "footer.php"; ?>
