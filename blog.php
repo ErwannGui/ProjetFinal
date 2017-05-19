820c83a24fbce9c584df228be2130a7aaa74fa72
AntoninDouillard <antonin.douillard@ynov.com>
2017-05-19 09:12:03 +0200

    Page blog

--- blog.php
+++ blog.php
@@ -36,7 +36,7 @@
                 </div>
 
                 <div id="blogCategories">
-                    <p>Afficher les résultats par catégories</p>
+                    <p>Afficher les articles par catégorie</p>
                     <button class="category" data-rel="all">Tout</button>
                     <button class="category" data-rel="cook">Cuisine</button>
                     <button class="category" data-rel="foodtruck">Food truck</button>
@@ -92,15 +92,49 @@
                 </div>
             </div>
 
-        </div>
+            <div id="blogTestimony">
 
-        <div id="blogTestimony">
+                <div id="blogTestimonyTitle">
+                    <h2>Témoignages</h2>
+                </div>
+                
+                <div id="blogTestimonyTestimonials">
 
-        </div>
+                    <div>
+                        <img src="images/testimonials/testimonyOne.jpg" alt="Témoignage" title="Témoignage">
+                        <h3>Vald</h3>
+                        <h5>Le 18/05/2017</h5>
+                        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet venenatis mauris sed posuere. Phasellus ut hendrerit mi, eu dignissim ligula. Cras fermentum lacus metus, vel facilisis mauris blandit vitae."</p>
+                    </div>
+                    <div>
+                        <img src="images/testimonials/testimonyTwo.jpg" alt="Témoignage" title="Témoignage">
+                        <h3>Lorenzo</h3>
+                        <h5>Le 18/05/2017</h5>
+                        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet venenatis mauris sed posuere. Phasellus ut hendrerit mi, eu dignissim ligula. Cras fermentum lacus metus, vel facilisis mauris blandit vitae."</p>
+                    </div>
+                    <div>
+                        <img src="images/testimonials/testimonyThree.jpg" alt="Témoignage" title="Témoignage">
+                        <h3>Columbine</h3>
+                        <h5>Le 18/05/2017</h5>
+                        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet venenatis mauris sed posuere. Phasellus ut hendrerit mi, eu dignissim ligula. Cras fermentum lacus metus, vel facilisis mauris blandit vitae."</p>
+                    </div>
 
+                </div>
 
-
-
+            </div>
+            
+            <div id="blogInformations">
+                <h2>Informations pratiques</h2>
+                <a href="mailto:ADRESSEGMAIL">Mail : adresse mail</a>
+                <a href="tel:0600000000">Téléphone : 06 00 00 00 00</a>
+                <a href="#"><svg enable-background="new 0 0 512 512" id="Layer_1" version="1.1" viewBox="0 0 512 512" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><path d="M256.108,3.02c-139.743,0-253,113.257-253,253s113.257,252.995,253,252.995   c139.743,0,253-113.252,253-252.995S395.852,3.02,256.108,3.02z M256.108,488.775c-128.338,0-232.76-104.417-232.76-232.755   c0-128.339,104.422-232.76,232.76-232.76c128.338,0,232.76,104.421,232.76,232.76C488.868,384.358,384.446,488.775,256.108,488.775   z"/><polygon points="266.228,104.22 245.988,104.22 245.988,245.9 104.98,245.9 104.98,266.14 245.988,266.14    245.988,407.148 266.228,407.148 266.228,266.14 407.908,266.14 407.908,245.9 266.228,245.9  "/></g></svg></a>
+                <a href="#">Plus d'informations</a>
+            </div>
+        
+        </div>  
+        
+        <?php include "footer.php"; ?>
+        
     </body>
 
 </html>
