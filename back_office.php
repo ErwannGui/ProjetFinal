87824af84f9d5a54899a244a542dffc97c59a1b5
ErwannGui <erwann.guillevic@ynov.com>
2017-05-20 18:34:04 +0200

    back-office contact
    Voila !! Comme on peut pas faire un simple iframe de gmail (ca serait
    trop facile) bah j'ai simulé une boite mail basique

--- back_office.php
+++ back_office.php
@@ -9,7 +9,7 @@
 		<?php include "headerBack.php"; ?>
 		<div id="containerBack">
 			<h1>Administration</h1>
-			<div class="back_content back_menu active">
+			<div class="back_content back_menu">
 				<div class="back_left_content">
 					<div class="back_top_left_content">
 						<h2>Ajouter un nouvel aliment</h2>
@@ -270,8 +270,46 @@
 					</div>
 				</div>
 			</div>
-			<div class="back_content back_contact">
-				<iframe id="boite_mail" src="https://www.google.com/intl/fr/gmail/about/"></iframe>
+			<div class="back_content back_contact active">
+				<!--<iframe id="boite_mail" src="https://www.google.com/intl/fr/gmail/about/"></iframe>-->
+				<?php
+					try
+					{
+						$bdd = new PDO('mysql:host=localhost;dbname=food_truck;charset=utf8', 'root', '');
+					}
+					catch(Exception $e)
+					{
+					        die('Erreur : '.$e->getMessage());
+					}
+					$messagereponse = $bdd->query('SELECT * FROM message');
+					while ($messagedonnees = $messagereponse->fetch())
+					{
+				?>
+				<div class="contactMessage ">
+					<div class="messageHeader">
+						<div class="messageHeaderLeft">
+							<p><?php echo $messagedonnees['messageNomEnvoyeur']; ?></p>
+						</div>
+						<div class="messageHeaderRight">
+							<p><?php echo $messagedonnees['messageObjet']; ?></p>
+							<!--<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="caret_down" x="0px" y="0px" width="25px" height="25px" viewBox="0 0 292.362 292.362" style="enable-background:new 0 0 292.362 292.362;" xml:space="preserve">
+								<g><path d="M286.935,69.377c-3.614-3.617-7.898-5.424-12.848-5.424H18.274c-4.952,0-9.233,1.807-12.85,5.424   C1.807,72.998,0,77.279,0,82.228c0,4.948,1.807,9.229,5.424,12.847l127.907,127.907c3.621,3.617,7.902,5.428,12.85,5.428   s9.233-1.811,12.847-5.428L286.935,95.074c3.613-3.617,5.427-7.898,5.427-12.847C292.362,77.279,290.548,72.998,286.935,69.377z" fill="#FFFFFF"/></g>
+							</svg>-->
+						</div>
+					</div>
+					<div class="messageBody">
+						<div class="messageBodyInfos">
+							<p><a href="mailto:chezfifi.resto@gmail.com"><?php echo $messagedonnees['messageMail']; ?></a> | <a href="tel:<?php echo $messagedonnees['messageTel']; ?>"><?php echo $messagedonnees['messageTel']; ?></a></p>
+						</div>
+						<div class="messageBodyContent">
+							<p><?php echo $messagedonnees['messageContenu']; ?></p>
+						</div>
+					</div>
+				</div>
+				<?php
+					}
+					$messagereponse->closeCursor();
+				?>
 			</div>
 			<div class="back_content back_reseau">
 				<span>Cette page est en cours de développement</span>
@@ -280,6 +318,7 @@
 	</body>
 	<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 	<script type="text/javascript">
+		/* --- Script header --- */
 		$('.btn_back_menu').click(function(){
 			$('.btn_header_back').removeClass('actif');
 			$(this).addClass('actif');
@@ -327,6 +366,10 @@
 			$(this).addClass('actif');
 			$('.back_content').removeClass('active');
 			$('.back_reseau').addClass('active');
+		});
+		/* --- Script Contact --- */
+		$('.contactMessage').click(function(){
+			$(this).toggleClass('visibleBody');
 		});
 	</script>
 </html>
