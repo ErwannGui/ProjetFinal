540b2339305157d66e43b755b5b24d668df4297c
Shadowkyu <quentin.dijoux@ynov.com>
2017-05-22 09:25:46 +0200

    QD acceuil

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
 		<title>Accueil</title>
         <meta charset="utf-8"/>
         <link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
+        <link rel="stylesheet" type="text/css" href="slick/slick/slick.css"/>
+        <link rel="stylesheet" type="text/css" href="slick/slick/slick-theme.css"/>
 	</head>
 	<body>
         
@@ -11,20 +13,20 @@
         include "headerFront.php"; ?>
 		
         <div id="container">
-            <div id="blogBackground">
+            <div id="indexBackground">
 
                 <div id="backgroundImage">
                 </div>
-                <div class="backgroundTitle">
-                    <h1>Acceuil</h1>
+                <div id="indexBackgroundTitle">
+                    <h1>ACCEUIL</h1>
                 </div>
-                <div id="blogBackgroundArrow">
-                    <a href="#blogArticles"><svg enable-background="new 0 0 26 26" id="Layer_1" version="1.1" viewBox="0 0 26 26" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><polygon points="0.046,2.582 2.13,0.498 12.967,11.334 23.803,0.498 25.887,2.582 12.967,15.502  "/><polygon points="0.046,13.582 2.13,11.498 12.967,22.334 23.803,11.498 25.887,13.582 12.967,26.502"/></g></svg></a>
+                <div id="indexBackgroundArrow">
+                    <a class="js-scroll" href="#scroll_ancre"><svg enable-background="new 0 0 26 26" id="Layer_1" version="1.1" viewBox="0 0 26 26" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><polygon points="0.046,2.582 2.13,0.498 12.967,11.334 23.803,0.498 25.887,2.582 12.967,15.502  "/><polygon points="0.046,13.582 2.13,11.498 12.967,22.334 23.803,11.498 25.887,13.582 12.967,26.502"/></g></svg></a>
                 </div>
             </div>
             <div class="index-container" >
-                <div class=text-container">
-                    <h2> Description </h2>
+                <div class="text-container">
+                    <h2> DESCRIPTION </h2>
                         <p> Lorem ipsum dolor sit amet, consectetuer adipiscing elit.Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis
                             parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
                             Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum
@@ -33,89 +35,113 @@
                             varius laoreet. </p>
                 </div>
                 <div class="image-container">
-                    <img src="images/blog_home.png">
+                    <div class="single-item">
+                        <div><img class="carousel1 carousel" src="images/index/carrousel01.png" alt="" title=""/></div>
+                        <div><img class="carousel2 carousel" src="images/index/carrousel02.png" alt="" title=""/></div>
+                        <div><img class="carousel3 carousel" src="images/index/carrousel03.png" alt="" title=""/></div>
+                    </div>
+                    <script type="text/javascript" src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
+                    <script type="text/javascript" src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+                    <script type="text/javascript" src="slick/slick/slick.min.js"></script>
+                    <script src="https://npmcdn.com/isotope-layout@3.0.1/dist/isotope.pkgd.js"></script>
+                    <script src="js/caroussel.js"></script>
                 </div>
                 <div class="formules">
-                    <h2>Nos formules</h2>
-                    <div>
-                       <img src="#">
-                       <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis
+                    <h2>NOS SPECIALITES</h2>
+                    <div class="bouchons">
+                       <img src="images/index/pic_BouchonGratine.png">
+                       <div class="description">
+                           <h3> BOUCHON GRATINE</h3>
+                           <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis
                            parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec</p>
+                           <a href="#"> En savoir plus </a>
+                       </div>
                     </div>
-                    <div>
-                        <img src="#">
-                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis
+                    <div class="rougail">
+                        <div class="description">
+                            <h3> ROUGAIL SAUCISSE</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis
                             parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec</p>
+                            <a href="#"> En savoir plus </a>
+                        </div>
+                        <img src="images/index/pic_rougail.png">
+
                     </div>
-                    <div>
-                        <img src="#">
-                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis
-                            parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec</p>
-                    </div>
-                    <a href="#"> Nos Formules</a>
                 </div>
                 <div class="events">
-                    <h2>Events</h2>
-                    <h3>Interventions</h3>
-                    <div class="mariage">
-                        <svg></svg>
-                        <p> porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus
-                            varius laoreet.</p>
+                    <div class="title">
+                        <h2>EVENEMENTS SPECIAUX</h2>
+                        <h3>INTERVENTIONS</h3>
                     </div>
-                    <div class="anniversaire">
-                        <svg></svg>
-                        <p> porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus
-                            varius laoreet.</p>
+                    <div class="SvgIntervention">
+                        <img src="images/index/camion.svg">
+                        <img src="images/index/gateau.svg">
+                        <img src="images/index/coeur.svg">
                     </div>
-                    <div class="baptême">
-                        <svg></svg>
+                    <div class="TextIntervention">
                         <p> porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus
+                            varius laoreet.porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus
+                            varius laoreet.porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus
+                            varius laoreet.porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus
                             varius laoreet.</p>
+                        <a href="#"> En savoir plus</a>
                     </div>
-                    <a href="#"> Evenements</a>
                 </div>
                 <div class="information">
-                    <div>
-                        <div>
-                            <p>Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
-                                Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo,
-                            </p>
+                    <div class="horairesacceuil">
+                        <div class="journéeacceuil">
+                            <div class="détails">
+                                <p class="jouracceuil">Mardi</p>
+                                <p class="adresseacceuil">8 boulevard des Pas Enchantés 44200 Nantes</p>
+                                <p>Midi :</p>
+                                <p>11h30 - 14h30</p>
+                                <p class="Soiracceuil">Soir :</p>
+                                <p>19h30 - 22h30</p>
+                            </div>
+                            <div class="détails">
+                                <p class="jouracceuil">Mercredi</p>
+                                <p class="adresseacceuil">20 Boulevard De Gaules 44200 Nantes</p>
+                                <p>Midi :</p>
+                                <p>11h30 - 14h30</p>
+                                <p class="Soiracceuil">Soir :</p>
+                                <p>19h30 - 22h30</p>
+                            </div>
+                            <div class="détails">
+                                <p class="jouracceuil">Jeudi</p>
+                                <p class="adresseacceuil">Adresse Compléte</p>
+                                <p>Midi :</p>
+                                <p>11h30 - 14h30</p>
+                                <p class="Soiracceuil">Soir :</p>
+                                <p>19h30 - 22h30</p>
+                            </div>
+                            <div class="détails">
+                                <p class="jouracceuil">Vendredi</p>
+                                <p class="adresseacceuil">Adresse Compléte</p>
+                                <p>Midi :</p>
+                                <p>11h30 - 14h30</p>
+                                <p class="Soiracceuil">Soir :</p>
+                                <p>19h30 - 22h30</p>
+                            </div>
                         </div>
-                        <div>
-                            <p>Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
-                                Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo,
-                            </p>
-                        </div>
-                        <div>
-                            <p>Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
-                                Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo,
-                            </p>
-                        </div>
-                        <div>
-                            <p>Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
-                                Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo,
-                            </p>
+                        <div class="contactacceuil">
+                            <h2>Informations pratiques</h2>
+                            <p>Mail : chezfifi@contact.com</p>
+                            <p> Tel : 06 - 00 - 00 - 00 - 00 </p>
                         </div>
                     </div>
-                    <div>
-                        <div>
-                            <p>Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
-                                Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo,
-                            </p>
-                        </div>
-                        <div>
-                            <p>Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
-                                Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo,
-                            </p>
-                        </div>
+                    <div class="Mapacceuil">
+                        <p> MAP </p>
                     </div>
-                    <p> MAP </p>
-                </div>
             </div>
-
 		</div>
+        </div>
         
 		<?php include "footer.php"; ?>
-        
+
+        <a href="#" class="top"><img src="images/fleche.png" title="Retour en haut"/></a>
+        <script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
+        <script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+        <script src="js/retourenhaut.js" type="text/javascript"></script>
+
 	</body>
 </html>
---
+++ js/caroussel.js
@@ -0,0 +1,6 @@
+$('.single-item').slick({
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+    arrows:false
+});
