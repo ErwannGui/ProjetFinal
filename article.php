8a2389bdbacd8ab7aff0e3f5628c4110c5653faa
AntoninDouillard <antonin.douillard@ynov.com>
2017-05-24 10:20:40 +0200

    ARTICLES FINIS !

--- article.php
+++ article.php
@@ -9,9 +9,9 @@
     $bdd ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
 
-<!DOCTYPE html>
+    <!DOCTYPE html>
 
-<html>
+    <html>
 
     <head>
         <meta charset="utf-8" />
@@ -23,7 +23,7 @@
 
         <?php $page = 4;
                 include "headerFront.php"; ?>
-        
+
         <?php $idarticle = $_GET['id'];
               $contenu= $bdd->query('SELECT articleTitre, texteId FROM article WHERE texteId ='.$idarticle);
         
@@ -48,50 +48,47 @@
                   "); } } 
         
         ?>
-           
-                
-        
-                <div id="articleBack">
-                    <a href="blog.php">Retour à la liste des articles</a>
-                </div>
-              
-             
-                <div id="blogArticlesArticles">
-                    <a href="article.php">
-                        <div>
-                            <article class="all foodtruck">
-                                <img src="images/articles/articleOne.jpg" alt="Food truck" title="Food truck">
-                                <p>Conseils pour bien gérer son food truck</p>
-                            </article>
-                        </div>
-                    </a>
-                    <a href="article.php">
-                        <div>
-                            <article class="all foodtruck cook">
-                                <img src="images/articles/articleTwo.jpg" alt="Food truck" title="Food truck">
-                                <p>Etre traiteur en food truck</p>
-                            </article>
-                        </div>
-                    </a>
-                    <a href="article.php">
-                        <div>
-                            <article class="all cook">
-                                <img src="images/articles/articleThree.jpg" alt="Food truck" title="Food truck">
-                                <p>La cuisine créole : nouvelle à Nantes</p>
-                            </article>
-                        </div>
-                    </a>
-                
-        </div>   <?php include "infos_comp.php"; ?>
-       <?php print_r("</div>") ?>
 
-           
 
-        
-<?php include "footer.php"; ?>
-     
-        
-        <a href="#" class="top"><img src="images/trash/fleche.png" title="Retour en haut"/></a>
+
+        <div id="articleBack">
+            <a href="blog.php">Retour à la liste des articles</a>
+        </div>
+
+
+        <div id="blogArticlesArticles">
+
+            <?php $article=$bdd->query('SELECT * FROM article ORDER BY RAND() LIMIT 3');
+                                  
+                            
+                            while($art=$article->fetch()){
+                                $imageArticle=$bdd->query("SELECT imageSource FROM Image WHERE imageId=" . $art['imageId']);
+                                
+                                while($imageArt=$imageArticle->fetch()){
+                                    
+                                    print_r(" 
+                                    
+                                        <a href='article.php?id=". $art['texteId'] ."' >
+                                            <div>
+                                                <article class=" /*. $art['ArticleCategorieAll'] */ . $art['ArticleCategorie'] . ">
+                                                    <img src=" . $imageArt['imageSource'] . " alt=" . $art['ArticleCategorie'] . " title=" . $art['ArticleCategorie'] . ">
+                                                    <p>" . $art['articleTitre'] . "</p>
+                                                </article>
+                                            </div>
+                                        </a>
+                                    "); } } ?>
+        </div>
+
+        <?php include "infos_comp.php"; ?>
+        <?php print_r("</div>") ?>
+
+
+
+
+        <?php include "footer.php"; ?>
+
+
+        <a href="#" class="top"><img src="images/trash/fleche.png" title="Retour en haut" /></a>
     </body>
 
-</html>
+    </html>
